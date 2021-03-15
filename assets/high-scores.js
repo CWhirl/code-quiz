4f97a07aeb9871cedd5d0e5ff2196db6f2c0e3e8
Camden Whirl <camdenwhirl@gmail.com>
2021-03-14 20:58:51 -0600

    fixed the design and logic of the high-scores page

--- assets/high-scores.js
+++ assets/high-scores.js
@@ -1,14 +1,6 @@
 var initial = localStorage.getItem("initials");
 var score = localStorage.getItem("score")
 
-if($('#initialOne').text() == "") {
-    $('#initialOne').text(initial);
-    $('#highScoreOne').text(score);
-} else if ($('#initialTwo').text() == "") {
-    $('#initialTwo').text(initial);
-    $('#highScoreTwo').text(score);
-} else {
-    $('#initialThree').text(initial);
-    $('#highScoreThree').text(score);
-}
+$('#initialOne').text(initial);
+$('#highScoreOne').text(score);
 
